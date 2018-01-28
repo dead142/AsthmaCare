c06799f1ea04d404b2cf4fc197758ef3051e1136
Andrei V. Berezhkov <dead0343@gmail.com>
2018-01-28 14:58:53 +0300

    extend User Dectrium Model

--- models/User.php
+++ models/User.php
@@ -2,103 +2,29 @@
 
 namespace app\models;
 
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
+use yii\db\Query;
+
+class User extends \dektrium\user\models\User
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
     /**
-     * @inheritdoc
+     * @param $role
+     * @return array
+     * @throws \yii\db\Exception
      */
-    public static function findIdentity($id)
+    public function getUsersByRole($role)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
-    }
+        $query = new Query;
+        $query->select('auth_assignment.user_id')
+              ->from('auth_assignment')
+              ->where(['item_name' => $role]);
 
-    /**
-     * @inheritdoc
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
+        $command = $query->createCommand();
+        $result = $command->queryAll();
+        if ($result) {
+            foreach ($result as $item) {
+                $finalResult[] = $item['user_id'];
             }
         }
-
-        return null;
-    }
-
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
+        return $finalResult;
     }
 }
